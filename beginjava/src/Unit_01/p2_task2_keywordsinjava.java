2dd51b8ee818e8d9226787480e2cd1f77e65fe7a
Dhananjay Badoni <dhananjaybadoni@gmail.com>
2022-04-10 09:39:27 +0530

    task2 commit

---
+++ beginjava/src/Unit_01/p2_task2_keywordsinjava.java
@@ -0,0 +1,16 @@
+package Unit_01;
+
+public class p2_task2_keywordsinjava {
+		
+		protected static final double PRICE =234.90;
+		public static final double PRICE2 = 234.90;
+		
+		static final int a = 10;
+		
+		public static void main(String[] args) {
+			
+			System.out.println(PRICE);
+			
+			System.out.println(a);
+}
+}
